b977a318ed3e44970a9fa06323ad0b54d512ebb8
viveksinghn7 <work.viveksinghnegi@gmail.com>
2024-09-12 07:40:08 +0530

    Initial commit

--- app.js
+++ app.js
@@ -132,6 +132,6 @@ app.get("/about", function(req, res){
 });
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000,'0.0.0.0', function () {
   console.log("Server is running on port " + (process.env.PORT || 3000));
 });
